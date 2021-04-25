8f34075f8dbce3a3e523618445f39d4cca082398
Trishul-Dhakulkar <82810595+Trishul-Dhakulkar@users.noreply.github.com>
2021-04-26 00:52:08 +0530

    Add files via upload

---
+++ Documentation/DB_Script/ShopBridge_DBScript.sql
@@ -0,0 +1,24 @@
+-- Create KDLDatabase
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'ShopBridge')
+  BEGIN
+	PRINT 'Creating ShopBridge Database';
+    CREATE DATABASE [ShopBridge]
+   END
+
+-- Create KDLUserData Table
+USE ShopBridge 
+IF NOT EXISTS(SELECT 1 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_SCHEMA = 'dbo' 
+AND  TABLE_NAME = 'Product')
+BEGIN
+ PRINT 'Creating Product table';
+CREATE TABLE Product
+( 
+ID INT IDENTITY(1,1) PRIMARY KEY,
+Name VARCHAR(50),
+Discription VARCHAR(200),
+Price FLOAT,
+)
+END
+
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+----Steps to Run the Shop Bridge Project----
+1] Run Database queries from DB_Script folder under Documentation.
+
+2] Install RestClient plugin in Firefox Browser.
+
+3] Get API Documentation ShopBridge_Doc.pdf and fallow documentation for API consumption.
+
+4] In-Case of any issues in API consumption using REST Client get reference of provided ScreenShots.
